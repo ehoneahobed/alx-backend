2e01cdf6a02165a6584c4eb07fe72200eeaefc97
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-17 21:17:25 +0000

    Fix styles

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -19,6 +19,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start index and an end index corresponding to the range of
     """
     # if page is 1, start at 0 and end at page_size
-    # if page is 2, start at ((page-1) * page_size) and end at (page_size * page)
-    # if page is 3, start at ((page-1) * page_size) and end at (page_size * page)
+    # if page is 2, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    # if page is 3, start at ((page-1) * page_size) and
+    # end at (page_size * page)
     return ((page-1) * page_size, page_size * page)
