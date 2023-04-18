1962fc9cdaea83b2db011e61658cfa54f21c091d
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-18 20:52:50 +0000

    Change default value for the function

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,7 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
+    def get_hyper_index(self, index: int = 1,
                         page_size: int = 10) -> Dict:
         """ return all data"""
 
