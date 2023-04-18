3ce1742323328c349a708cb81d05f9d84e242e45
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-18 22:55:55 +0000

    Add solution to task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Create a class FIFOCache that inherits from
+"""Create a class LIFOCache that inherits from
 BaseCaching and is a caching system:
 
 You must use self.cache_data - dictionary from
@@ -7,14 +7,14 @@ the parent class BaseCaching
 You can overload def __init__(self): but don’t
 forget to call the parent init: super().__init__()
 def put(self, key, item):
-Must assign to the dictionary self.cache_data
-the item value for the key key.
-If key or item is None, this method should not
-do anything.
-If the number of items in self.cache_data is
-higher that BaseCaching.MAX_ITEMS:
-you must discard the first item put in cache
-(FIFO algorithm)
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the last item put in cache
+(LIFO algorithm)
 you must print DISCARD: with the key discarded and
 following by a new line
 def get(self, key):
@@ -27,7 +27,7 @@ self.cache_data, return None.
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """_summary_
     """
 
@@ -48,9 +48,9 @@ class FIFOCache(BaseCaching):
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data.keys():
-                first_key = next(iter(self.cache_data.keys()))
-                del self.cache_data[first_key]
-                print("DISCARD: {}". format(first_key))
+                # delete the last item in the dictionary
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
 
             self.cache_data[key] = item
 
