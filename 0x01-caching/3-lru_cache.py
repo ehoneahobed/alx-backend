1b742a7a61ff3287260ad01460378502dbea293d
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-20 03:06:51 +0000

    Update solution to task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,47 +1,68 @@
-#!/usr/bin/python3
-''' LRU Caching: Create a class LRUCache that inherits from BaseCaching
-                 and is a caching system
-'''
+#!/usr/bin/env python3
+"""Create a class LRUCache that inherits from
+BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the least recently used item
+(LRU algorithm)
+you must print DISCARD: with the key discarded
+and following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
+
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    ''' An LRU Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the least recently used entry to
-        accommodate for the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
+    """_summary_
+    """
 
     def __init__(self):
-        ''' Initialize class instance. '''
+        """_summary_
+        """
         super().__init__()
-        self.keys = []
+        self.usedKeys = []
 
     def put(self, key, item):
-        ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard least recently used entry in cache to accommodate new
-            entry. '''
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
             else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(0)
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(0)
                 del self.cache_data[discard]
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
-            return self.cache_data[key]
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
+        """
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
         return None
