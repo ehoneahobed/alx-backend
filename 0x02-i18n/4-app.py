b9599be59aea23cb4f6a0a29c8f1361d105eb045
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-25 23:39:04 +0000

    Update solution to task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -8,14 +8,14 @@ from flask_babel import Babel
 
 
 class Config(object):
-	"""_summary_
+    """_summary_
 
-	Returns:
-		_type_: _description_
-	"""
-	LANGUAGES = ['en', 'fr']
-	BABEL_DEFAULT_LOCALE = 'en'
-	BABEL_DEFAULT_TIMEZONE = 'UTC'
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 # configure the flask app
@@ -27,24 +27,24 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-	"""_summary_
+    """_summary_
 
-	Returns:
-		_type_: _description_
-	"""
-	locale = request.args.get('locale')
-	if locale in app.config['LANGUAGES']:
-		return locale
+    Returns:
+            _type_: _description_
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
 
-	return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-	"""_summary_
-	"""
-	return render_template('4-index.html')
+    """_summary_
+    """
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-	app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
