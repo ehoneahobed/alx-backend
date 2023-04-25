65d2faae7d122967d78e8404153909cdd9489413
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-25 23:37:51 +0000

    Update solution to task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,47 +1,50 @@
 #!/usr/bin/env python3
+"""A simple flask app
 """
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+
+
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
-    """
-    Configuration for Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+	"""_summary_
+
+	Returns:
+		_type_: _description_
+	"""
+	LANGUAGES = ['en', 'fr']
+	BABEL_DEFAULT_LOCALE = 'en'
+	BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    loc = request.args.get('locale')
-    if loc in app.config['LANGUAGES']:
-        return loc
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+	"""_summary_
+
+	Returns:
+		_type_: _description_
+	"""
+	locale = request.args.get('locale')
+	if locale in app.config['LANGUAGES']:
+		return locale
+
+	return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('4-index.html')
+@app.route('/')
+def index():
+	"""_summary_
+	"""
+	return render_template('4-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+	app.run(port="5000", host="0.0.0.0", debug=True)
