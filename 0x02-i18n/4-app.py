7a0ca7917514cb36c055a354e7f5a774b0af763c
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-26 00:00:50 +0000

    Update solution to task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,7 +25,7 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale():
     """_summary_
 
@@ -34,9 +34,12 @@ def get_locale():
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
+        print(locale)
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
